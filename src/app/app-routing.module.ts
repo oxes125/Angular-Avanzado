1439a8fa4f3841ef916bfe7c2a355788df6eeda6
oxes125 <oxes125@hotmail.com>
2022-09-02 15:29:01 -0500

    seccion 4 end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopageFoundComponent } from './pages/nopage-found/nopage-found.component';
-import { PagesComponent } from './pages/pages.component';
+import { AuthRouringmodule } from './auth/auth.routing.module';
+
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+import { pagesRoutingModule } from './pages/pages.routing';
 
 
 
 const routes:Routes=[ //PagesComponent tiene los  <router-outlet> <app-sidebar> <app-header> asi que tiene todos los estilos y los embebe
-{path:'', component: PagesComponent ,//rutas protegidas con estilo o template, cumplen condiciones de disenio es la misma
-children:[
-  {path:'dashboard', component: DashboardComponent},
-  {path:'progress', component:ProgressComponent},
-  {path:'grafica1', component:Grafica1Component},
-  {path:'', redirectTo:'/dashboard', pathMatch:'full'},
-  
-]
-}, 
-
-{path: 'login', component: LoginComponent},     //sin estylo 
-{path:'register', component:RegisterComponent},
-
+{path:'', redirectTo:'dashboard', pathMatch: 'full'},
 {path:'**', component: NopageFoundComponent},
 ]
 
@@ -31,7 +16,9 @@ children:[
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    pagesRoutingModule,  ///rutas por modulo importar el routing
+    AuthRouringmodule
   ],
   exports:[
     RouterModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopageFoundComponent } from './pages/nopage-found/nopage-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './shares/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shares/sidebar/sidebar.component';
-import { HeaderComponent } from './shares/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+
 import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages.component';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopageFoundComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
